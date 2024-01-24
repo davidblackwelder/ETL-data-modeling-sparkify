caa704845d12b537e7daa9a2f729a4c535e6729d
davidblackwelder <github@davidblackwelder.com>
2024-01-24 04:02:01 -0500

    Get table attributes ready and secure database connection details

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,40 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays"
-user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = ("CREATE TABLE songplays ( \
+                           songplay_id
+        start_time
+        user_id,
+        level
+        song_id
+        artist_id
+        session_id
+        location
+        user_agent
+    )
+")
 
 user_table_create = ("""
+user_id, first_name, last_name, gender, level
 """)
 
 song_table_create = ("""
+song_id, title, artist_id, year, duration
 """)
 
 artist_table_create = ("""
+artist_id, name, location, latitude, longitude
 """)
 
 time_table_create = ("""
+start_time, hour, day, week, month, year, weekday
 """)
 
 # INSERT RECORDS
