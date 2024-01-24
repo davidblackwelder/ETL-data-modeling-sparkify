ded0364b6b6c8f3ee10e073e2ef3e69d2c4a51ca
davidblackwelder <github@davidblackwelder.com>
2024-01-24 07:02:03 -0500

    Finish code for creating database and tables. Update test notebook to ensure tests run successfully

--- create_tables.py
+++ create_tables.py
@@ -1,4 +1,6 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 from sql_queries import create_table_queries, drop_table_queries
 
 
@@ -7,24 +9,45 @@ def create_database():
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
+    # Load .env variables
+    load_dotenv()
+
+    # Store .env variables for default database
+    # Keeps database credentials hidden
+    HOST = os.getenv('HOST')
+    DEFAULT_DB_NAME = os.getenv('DEFAULT_DBNAME')
+    DEFAULT_USER = os.getenv('DEFAULT_USER')
+    DEFAULT_PASSWORD = os.getenv('DEFAULT_PASSWORD')
+
+    # Creates connection string
+    default_conn_string = f'host={HOST} dbname={
+        DEFAULT_DB_NAME} user={DEFAULT_USER} password={DEFAULT_PASSWORD}'
 
     # connect to default database
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn = psycopg2.connect(default_conn_string)
     conn.set_session(autocommit=True)
     cur = conn.cursor()
 
     # create sparkify database with UTF8 encoding
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute(
-        "CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+        "CREATE DATABASE sparkifydb WITH ENCODING 'utf8'")
 
     # close connection to default database
     conn.close()
 
+    # Store .env variables for sparkify database
+    # Keeps database credentials hidden
+    DB_NAME = os.getenv('DBNAME')
+    USER = os.getenv('USER')
+    PASSWORD = os.getenv('PASSWORD')
+
+    # Creates connection string
+    conn_string = f'host={HOST} dbname={
+        DB_NAME} user={USER} password={PASSWORD}'
+
     # connect to sparkify database
-    conn = psycopg2.connect(
-        "host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect(conn_string)
     cur = conn.cursor()
 
     return cur, conn
--- sql_queries.py
+++ sql_queries.py
@@ -8,61 +8,98 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("CREATE TABLE songplays ( \
-                           songplay_id
-        start_time
-        user_id,
-        level
-        song_id
-        artist_id
-        session_id
-        location
-        user_agent
-    )
-")
-
 user_table_create = ("""
-user_id, first_name, last_name, gender, level
-""")
-
-song_table_create = ("""
-song_id, title, artist_id, year, duration
+    CREATE TABLE users (
+      user_id INT,
+      first_name VARCHAR NOT NULL,
+      last_name VARCHAR NOT NULL,
+      gender CHAR(1),
+      level VARCHAR(10) NOT NULL,
+      PRIMARY KEY (user_id)
+    );
 """)
 
 artist_table_create = ("""
-artist_id, name, location, latitude, longitude
+    CREATE TABLE artists (
+       artist_id VARCHAR,
+       name VARCHAR NOT NULL,
+       location VARCHAR,
+       latitude NUMERIC,
+       longitude NUMERIC,
+       PRIMARY KEY (artist_id)
+    );
+""")
+
+song_table_create = ("""
+    CREATE TABLE songs (
+        song_id VARCHAR,
+        title VARCHAR NOT NULL,
+        artist_id VARCHAR NOT NULL,
+        year INT NOT NULL,
+        duration NUMERIC NOT NULL,
+        PRIMARY KEY (song_id),
+        FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    );
 """)
 
 time_table_create = ("""
-start_time, hour, day, week, month, year, weekday
+    CREATE TABLE time (
+        start_time TIMESTAMP,
+        hour INT,
+        day INT,
+        week INT,
+        month INT,
+        year INT,
+        weekday INT,
+        PRIMARY KEY (start_time)
+    );
+""")
+
+songplay_table_create = ("""
+    CREATE TABLE songplays (
+      songplay_id SERIAL,
+      start_time TIMESTAMP NOT NULL,
+      user_id INT NOT NULL,
+      level VARCHAR(10) NOT NULL,
+      song_id VARCHAR NOT NULL,
+      artist_id VARCHAR NOT NULL,
+      session_id INT NOT NULL,
+      location VARCHAR NOT NULL,
+      user_agent VARCHAR NOT NULL,
+      PRIMARY KEY (songplay_id),
+      FOREIGN KEY (user_id) REFERENCES users (user_id),
+      FOREIGN KEY (song_id) REFERENCES songs (song_id),
+      FOREIGN KEY (artist_id) REFERENCES artists (artist_id),
+      FOREIGN KEY (start_time) REFERENCES time (start_time)
+    );
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+# songplay_table_insert = ("""
+# """)
 
-user_table_insert = ("""
-""")
+# user_table_insert = ("""
+# """)
 
-song_table_insert = ("""
-""")
+# song_table_insert = ("""
+# """)
 
-artist_table_insert = ("""
-""")
+# artist_table_insert = ("""
+# """)
 
 
-time_table_insert = ("""
-""")
+# time_table_insert = ("""
+# """)
 
-# FIND SONGS
+# # FIND SONGS
 
-song_select = ("""
-""")
+# song_select = ("""
+# """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create,
-                        song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop,
-                      song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, artist_table_create,
+                        song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, artist_table_drop,
+                      song_table_drop, time_table_drop, songplay_table_drop]
